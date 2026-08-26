92e473ddab8adfb4b145c6386cc455aabeda252a
AnaLeria <leriaanairis@gmail.com>
2026-08-25 20:38:04 -0400

    feat: agregar link a tareas en tarjeta de proyecto

--- src/modules/projects/ProjectCard.tsx
+++ src/modules/projects/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import type { Project } from '../../types/project'
 import { DeleteProjectButton } from './DeleteProjectButton'
 
@@ -12,6 +13,7 @@ export const ProjectCard: React.FC<Props> = ({ project, onEdit, onDelete, isAdmi
   return (
     <div>
       <h3>{project.name}</h3>
+      <Link to={`/projects/${project.id}/tasks`}>Ver tareas</Link>
       {project.description !== null && <p>{project.description}</p>}
       <button type="button" onClick={() => onEdit(project)}>Editar</button>
       {isAdmin && <DeleteProjectButton project={project} onDelete={onDelete} />}
